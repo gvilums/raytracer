93afafc329085f61696003ba96040bcc5d36dc20
Georgijs Vilums <georgijs.vilums@gmail.com>
2020-02-13 22:36:55 +0200

    planar projection view added

--- src/library/light.rs
+++ src/library/light.rs
@@ -1,12 +1,58 @@
 use nalgebra::Vector3;
 
-pub struct Light {
+pub struct Lighting {
+    pub points: Vec<PointLight>,
+    pub globals: Vec<GlobalLight>,
+    pub point_inv_pot: Vector3<f64>,
+    pub global_inv_pot: Vector3<f64>,
+    pub point_global_ratio: f64,
+}
+
+impl Lighting {
+    pub fn new(ratio: f64) -> Self {
+        Lighting {
+            points: Vec::new(),
+            globals: Vec::new(),
+            point_inv_pot: Vector3::new(1f64, 1f64, 1f64),
+            global_inv_pot: Vector3::new(1f64, 1f64, 1f64),
+            point_global_ratio: ratio,
+        }
+    }
+
+    pub fn add_point(&mut self, pos: Vector3<f64>, color: Vector3<f64>) {
+        self.points.push(PointLight { pos, color });
+        for i in 0..3 {
+            self.point_inv_pot[i] = 1. / (1. / self.point_inv_pot[i] + color[i]);
+        }
+    }
+
+    pub fn add_global(&mut self, dir: Vector3<f64>, color: Vector3<f64>) {
+        self.globals.push(GlobalLight { dir, color });
+        for i in 0..3 {
+            self.global_inv_pot[i] = 1. / (1. / self.global_inv_pot[i] + color[i]);
+        }
+    }
+}
+
+
+pub struct PointLight {
     pub pos: Vector3<f64>,
     pub color: Vector3<f64>,
 }
 
-impl Light {
+impl PointLight {
     pub fn new(pos: Vector3<f64>, color: Vector3<f64>) -> Self {
-        Light { pos, color }
+        PointLight { pos, color }
+    }
+}
+
+pub struct GlobalLight {
+    pub dir: Vector3<f64>,
+    pub color: Vector3<f64>,
+}
+
+impl GlobalLight {
+    pub fn new(dir: Vector3<f64>, color: Vector3<f64>) -> Self {
+        GlobalLight { dir, color }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use crate::library::object::{sphere::Sphere, Object};
 use nalgebra::Vector3;
-use crate::library::tracer::{trace_all, spherical_view};
-use crate::library::light::Light;
+use crate::library::tracer::{trace_all, spherical_view, planar_view};
+use crate::library::light::{PointLight, Lighting};
 use crate::library::object::plane::Plane;
 use image::Rgb;
 
 mod library;
 
+
 fn main() {
     let mut objects: Vec<Box<dyn Object + Sync>> = Vec::new();
-    let mut s1 = Sphere::new(Vector3::new(-15f64, -5f64, 100f64), 10.);
-    let mut s2 = Sphere::new(Vector3::new(60f64, 0f64, 150f64), 10.);
-    let mut s3 = Sphere::new(Vector3::new(35f64, -10f64, 75f64), 10.);
+    let mut s1 = Sphere::new(Vector3::new(-15f64, -30f64, 100f64), 10.);
+    let mut s2 = Sphere::new(Vector3::new(60f64, -30f64, 150f64), 10.);
+    let mut s3 = Sphere::new(Vector3::new(25f64, -30f64, 75f64), 10.);
     s3.set_properties(Vector3::new(0.03, 0.03, 0.03), Vector3::new(0., 0.78, 0.34));
     s2.set_properties(Vector3::new(1., 0.78, 0.34), Vector3::new(0., 0., 0.));
     s1.set_properties(Vector3::new(0.08, 0.08, 0.08), Vector3::new(0.5, 0.8, 0.8));
@@ -25,30 +26,42 @@ fn main() {
     );
     p.set_properties(
         Vector3::new(0.01f64, 0.01f64, 0.01f64),
-        Vector3::new(0.6f64, 0.6f64, 0.6f64),
+        Vector3::new(0.8f64, 0.8f64, 0.8f64),
     );
 
     objects.push(Box::new(p));
 
 
-    let mut lights: Vec<Light> = Vec::new();
+    let mut lights = Lighting::new(0.3);
+    // local lighting around the scene
     for i in 1..20 {
         for j in -6..6 {
-            lights.push(
-                Light::new(
+            lights.add_point(
                     Vector3::new(3. * j as f64, 50f64, 3. * i as f64),
-                    Vector3::new(1f64, 1f64, 1f64)));
+                    Vector3::new(1f64, 0f64, 0f64)
+            );
+        }
+    }
+    // global lighting
+    for i in -2..=2 {
+        for j in 1..6 {
+            for k in -2..=2 {
+                lights.add_global(
+                    Vector3::new(0.1 * i as f64, -0.1 * j as f64, 0.1 * k as f64),
+                    Vector3::new(1f64, 1f64, 1f64),
+                )
+            }
         }
     }
 
     let width = 1600;
     let height = 1200;
 
-    // get a spherical view
-    let view = spherical_view(
+    // get a view
+    let view = planar_view(
         Vector3::new(1f64, 0f64, 0f64),
-        Vector3::new(0f64, 1f64, 0.2f64),
-        std::f64::consts::PI * 1. / 4.,
+        Vector3::new(0f64, 1f64, 0.3f64),
+        std::f64::consts::PI * 1. / 5.,
         width,
         height,
     );
@@ -56,7 +69,7 @@ fn main() {
     // calculate the colors of each pixel in the view
     let pixel_colors = trace_all(
         view,
-        Vector3::new(20f64, 30f64, -80f64),
+        Vector3::new(0f64, 20f64, -80f64),
         objects,
         lights,
     );
